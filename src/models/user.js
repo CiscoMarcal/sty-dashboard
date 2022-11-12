f62fcb210d74f80b3b0c085815d6045f1f628a60
Cisco <cisco.marcal.dev@gmail.com>
2022-11-12 12:41:00 -0300

    Implementação e do método para pegar as inicial do nome

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,15 @@ class User extends Model {
   static init(sequelize) {
     super.init({
       initials: {
-
+        type: Sequelize.VIRTUAL,
+        get() {
+          const mutch = this.name.split(" ");
+          if (mutch.length > 1) {
+            return `${mutch[0][0]}${mutch[mutch.length - 1][0]}}`;
+          } else {
+            return mutch[0][0];
+          }
+        }
       },
       name: Sequelize.STRING,
       email: Sequelize.STRING,
